d87dcfe100c1a845ea97ba4e542bf1f302b62cb8
Justin J Daniel <62233773+Justinjdaniel@users.noreply.github.com>
2020-06-08 18:48:54 +0530

    Home Page Checkbox Conditions re-structured.
    Small tweaks are done in the JScript.

--- JS/Home.js
+++ JS/Home.js
@@ -6,9 +6,14 @@ function List(){
         if (this.readyState == 4&&this.status==200){
             var list = JSON.parse(this.responseText);
             // console.log(list);
-            var todotab = "<tr style=\"text-align: center;\"><th>Status</th><th>Activities</th></tr>";
+            var todotab = "<tr style=\"text-align: center;\"><th>Status</th><th>Activities</th><th>Completed</th></tr>";
             for (var i=0;i<list.length;i++){
-                todotab += "<tr><td><input type=\"checkbox\" name=\"task\" style=\"margin-left: 40%;\"></td><td>"+ list[i].title +"</td></tr>";
+                if(list[i].completed == true){
+                    todotab += "<tr><td><input type=\"checkbox\" name=\"task\" disabled=\"\" style=\"margin-left: 40%;\"></td><td>"+ list[i].title +"</td><td>"+list[i].completed+"</td></tr>";
+                }
+                else{
+                    todotab += "<tr><td><input type=\"checkbox\" name=\"task\" style=\"margin-left: 40%;\"></td><td>"+ list[i].title +"</td><td>"+list[i].completed+"</td></tr>";
+                }
             }
             document.getElementById("table").innerHTML = todotab;
             let button = document.getElementById("showbtn");
@@ -25,7 +30,7 @@ function TaskStatus(){
     console.log(checkboxes);
     var promise = new Promise(function(resolve,reject){
         if(checkboxes.length > 4){
-            resolve("Congrats. "+checkboxes.length+" Tasks have been Successfully Completed");
+            resolve("Congrats. 5 Tasks have been Successfully Completed");
         }
         else{
             reject("Almost there, Keep Going...");
@@ -33,6 +38,9 @@ function TaskStatus(){
     })
     promise.then(function(done){
         alert(done);
+        document.querySelectorAll('input[name="task"]').forEach(element => {
+            element.setAttribute("disabled", "disabled");
+        });
     })
     .catch(function(undone){
         alert(undone);
